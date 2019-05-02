6e95ab75382f1690c82f8fb2cd2152fa6853a852
Charles Oroko <charles.oroko@outlook.com>
2019-05-02 11:43:12 -0700

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,18 +1,28 @@
-const colourLoversAPI = 'http://www.colorlovers.com/api/colors'
+const colourLoversAPI = "http://www.colourlovers.com/api/colors";
 
 $(document).ready(() => {
-  $('#searchBtn').on('click', () => {
-    let searchText = $('#searchText').value
-    let searchUrl = `${colourLoversAPI}?keywords=${searchText}&jsonCallback=?`
-    $.get(searchUrl, results => {
-      console.log('results: ', results)
-      const paletteTemplate = $('#palleteTemplate')
-      const output = $('#output')
+  $("#searchBtn").on("click", () => {
+    let searchText = $("#searchText").val();
+    let searchUrl = `${colourLoversAPI}?keywords=${searchText}&jsonCallback=?`;
 
-      results.foreach(palette => {
-        console.log('cloning')
-        output.append(paletteTemplate.html().trim())
-      })
-    })
-  })
-})
+    $.getJSON(searchUrl, results => {
+      const paletteTemplate = $("#paletteTemplate");
+      const paletteHtml = paletteTemplate.html().trim();
+      const output = $("#output");
+
+      results.forEach(palette => {
+        let $palette = $(paletteHtml);
+
+        let $paletteImage = $palette.find(".palette-image");
+        let $paletteName = $palette.find(".palette-name");
+        let $paletteAuthor = $palette.find(".palette-author");
+
+        $paletteImage.attr("src", palette.imageUrl);
+        $paletteName.text(palette.title);
+        $paletteAuthor.text(palette.userName);
+
+        output.append($palette);
+      });
+    });
+  });
+});
