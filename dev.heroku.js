0cf5ecf7f24f5ca487b0a343e9086f5ba6af44a6
Chiq2045 <charles.oroko@outlook.com>
2019-05-21 19:22:52 +0000

    trying to fix some errors on heroku

--- dev.heroku.js
+++ dev.heroku.js
@@ -3,7 +3,7 @@ const serve = require('koa-static');
 const dev = new Koa();
 const port = process.env.PORT || 8000;
 
-dev.use(serve(__dirname + '/dist'));
+dev.use(serve(__dirname + '/./dist'));
 
 dev.listen(port);
 
